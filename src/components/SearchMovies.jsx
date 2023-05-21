07e2bc7acc9c4b79eb403eba4e7ca5a4e7cb41fd
HtetKo510217 <htetko510217@gmail.com>
2023-05-21 21:12:03 +0630

    Show search movies result with CardBlog component

--- src/components/SearchMovies.jsx
+++ src/components/SearchMovies.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios';
+import CardBlog from './CardBlog';
+
 export default function SearchMovies() {
   const [query,setQuery] = useState('');
   const [movies,setMovies] = useState([]);
@@ -8,11 +10,8 @@ export default function SearchMovies() {
     e.preventDefault();
   
      const url = `https://api.themoviedb.org/3/search/movie?api_key=a0de9f0783ece73507c0c2c4f3b9c64e&language=en-US&query=${query}&page=1&include_adult=false`;
-
-    console.log('submitting');
     try {
       const response = await axios.get(url);
-      console.log(response.data.results);
       setMovies(response.data.results);
     } catch (error) {
       console.error(error);
@@ -23,20 +22,17 @@ export default function SearchMovies() {
   return (
     <>
       <form className="form" onSubmit={searchMovies}>
-                <label className="label" htmlFor="query">Movie Name</label>
-                <input className="input" type="text" name="query"
-                    placeholder="i.e. Jurassic Park"
-                    value={query} onChange={(e) => setQuery(e.target.value)}
-                    />
-                <button className="button" type="submit">Search</button>
-            </form>
+        <label className="label" htmlFor="query">Movie Name</label>
+        <input className="input" type="text" name="query"
+            placeholder="i.e. Jurassic Park"
+            value={query} onChange={(e) => setQuery(e.target.value)}
+            />
+        <button className="button" type="submit">Search</button>
+      </form>
       {movies.length > 0 && (
-        <ul>
-          {movies.map((movie) => (
-            <li key={movie.id}>{movie.name}</li>
-          ))}
-        </ul>
+        <CardBlog movies={movies} />
       )}
+      
     </>
   )
 }
